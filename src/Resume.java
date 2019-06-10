3032dfd834fa9a8ec4e9e6a86aa1f9a97e4e908c
Юрий <apach88@gmail.com>
2019-06-10 17:20:23 +0300

    ArrayStorage impl

--- src/Resume.java
+++ src/Resume.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable  {
 
     // Unique identifier
     String uuid;
@@ -9,5 +11,28 @@ public class Resume {
     @Override
     public String toString() {
         return uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(uuid, resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
