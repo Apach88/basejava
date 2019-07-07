fb846185002a9cb65309620fbebb64486e5c8ab5
Юрий <apach88@gmail.com>
2019-07-07 20:42:48 +0300

    HW4 ready to review

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -34,38 +34,38 @@ public abstract class AbstractArrayStorageTest {
     @Test
     public void clear() throws Exception {
         storage.clear();
-        Assert.assertEquals(0,storage.size());
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() throws Exception {
         Resume oldResume = storage.get(UUID_2);
         storage.update(new Resume(UUID_2));
-        Assert.assertNotSame(oldResume,storage.get(UUID_2));
+        Assert.assertNotSame(oldResume, storage.get(UUID_2));
     }
 
     @Test
     public void getAll() throws Exception {
-        Resume[] resumes = new Resume[]{new Resume(UUID_1),new Resume(UUID_2),new Resume(UUID_3)};
-        Assert.assertArrayEquals(resumes,storage.getAll());
+        Resume[] resumes = new Resume[]{new Resume(UUID_1), new Resume(UUID_2), new Resume(UUID_3)};
+        Assert.assertArrayEquals(resumes, storage.getAll());
     }
 
     @Test
     public void save() throws Exception {
         storage.save(new Resume("uuid4"));
-        Assert.assertEquals(4,storage.size());
-        Assert.assertEquals("uuid4",storage.get("uuid4").getUuid());
+        Assert.assertEquals(4, storage.size());
+        Assert.assertEquals("uuid4", storage.get("uuid4").getUuid());
     }
 
     @Test
     public void delete() throws Exception {
         storage.delete(UUID_1);
-        Assert.assertEquals(2,storage.size());
+        Assert.assertEquals(2, storage.size());
     }
 
     @Test
     public void get() throws Exception {
-        Assert.assertEquals(UUID_1,storage.get(UUID_1).getUuid());
+        Assert.assertEquals(UUID_1, storage.get(UUID_1).getUuid());
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -74,12 +74,12 @@ public abstract class AbstractArrayStorageTest {
     }
 
     @Test(expected = StorageException.class)
-    public void storageOverFlow(){
+    public void storageOverFlow() {
         try {
-            for (int i = 3;i<10000; i++) {
-                storage.save(new Resume("uuid"+i+1));
+            for (int i = 3; i < 10000; i++) {
+                storage.save(new Resume("uuid" + i + 1));
             }
-        }catch (StorageException e){
+        } catch (StorageException e) {
             Assert.fail("Storage will not be overflow");
         }
         storage.save(new Resume("overUuid"));
